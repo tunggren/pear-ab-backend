a7f601ee4b63073c82ee9390b9410a6de6f12e3d
Aron Ljunggren <aron.ljunggren@outlook.com>
2023-11-12 12:19:44 +0100

    added backend repo

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const app = express();
+const PORT = 3000;
+
+// Connect to MongoDB (Assuming you have MongoDB running locally)
+mongoose.connect('mongodb://localhost/inventory', { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Define a simple inventory item schema
+const inventorySchema = new mongoose.Schema({
+  name: String,
+  quantity: Number,
+});
+
+const Inventory = mongoose.model('Inventory', inventorySchema);
+
+app.use(bodyParser.json());
+
+// Get current inventory
+app.get('/api/inventory', async (req, res) => {
+  try {
+    const inventory = await Inventory.find();
+    res.json(inventory);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+// Update inventory
+app.post('/api/inventory', async (req, res) => {
+  try {
+    const { name, quantity } = req.body;
+    const existingItem = await Inventory.findOne({ name });
+
+    if (existingItem) {
+      existingItem.quantity += quantity;
+      await existingItem.save();
+    } else {
+      const newItem = new Inventory({ name, quantity });
+      await newItem.save();
+    }
+
+    res.sendStatus(200);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
